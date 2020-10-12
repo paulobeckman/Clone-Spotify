9b3e8bad5d753d2a364865c801599dabda68ef15
paulobeckman <beckmam.paulo@hotmail.com>
2020-10-12 18:58:27 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
+
+const server = express()
+
+server.use(express.urlencoded({extended:true}))
+server.use(express.static('public'))
+server.use(routes)
+
+server.set("view engine", "njk")
+
+nunjucks.configure("src/app/views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.listen(5000, function () {
+    console.log("server is running")
+})
