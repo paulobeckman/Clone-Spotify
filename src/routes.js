4e559a3c52de5ab73d93c53f6e0e5c013076baca
paulobeckman <beckmam.paulo@hotmail.com>
2020-10-13 18:18:10 -0300

    left bar completed

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req,res){
-    return res.render("layout.njk")
+    return res.render("home.njk")
 })
 
 module.exports = routes
