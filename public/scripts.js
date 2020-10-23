3d1877ab10e5f48178a14576ef8216ae4214c056
paulobeckman <beckmam.paulo@hotmail.com>
2020-10-23 16:07:10 -0300

    create html modal-overlay

--- public/scripts.js
+++ public/scripts.js
@@ -3,6 +3,6 @@ const menuItems = document.querySelectorAll(".itemLeftBar ul li a")
 
 for (item of menuItems) {
     if (currentPage.includes(item.getAttribute("href"))){
-        item.classList.add("active")
+        item.classList.add("")
     }
 }
