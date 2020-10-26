a37a54e46a12129dba011ca3290e593b6c0a6714
paulobeckman <beckmam.paulo@hotmail.com>
2020-10-26 19:58:19 -0300

    create style modal-overlay

--- public/scripts.js
+++ public/scripts.js
@@ -3,6 +3,19 @@ const menuItems = document.querySelectorAll(".itemLeftBar ul li a")
 
 for (item of menuItems) {
     if (currentPage.includes(item.getAttribute("href"))){
-        item.classList.add("")
+        item.classList.add("active")
     }
 }
+
+const modalOverlay = document.querySelector(".modal-overlay")
+const criar = document.querySelector(".itemLeftBar.sectionMenuPlaylist ul li")
+const close = document.querySelector(".close-modal")
+
+criar.addEventListener("click", function(){
+    modalOverlay.classList.add('active')
+})
+
+close.addEventListener("click", function(){
+    modalOverlay.classList.remove('active')
+})
+
